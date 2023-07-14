5be9936c01bb5a9996dcc7b89ea52092fac32570
yArna <82231420+yArna@users.noreply.github.com>
2023-07-14 12:34:02 +0800

    Add files via upload

---
+++ ttc-to-ttf.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from fontTools.ttLib.ttCollection import TTCollection
+import os
+import sys
+
+filename = sys.argv[1]
+ttc = TTCollection(filename)
+print(sys.argv[1])
+basename = os.path.basename(filename)
+for i, font in enumerate(ttc):
+    font.save(f"otf/{font['name'].getBestFullName()}.otf")
